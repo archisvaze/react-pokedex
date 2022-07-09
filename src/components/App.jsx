1072bac0b0097041905dfe2ff2eefba714a82f8a
archis <vazearchis@gmail.com>
2022-07-09 22:37:39 +0530

    done

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Pokemon from "./Pokemon";
+import PokemonLogo from "../pokemon-logo.png"
+
+function App(props) {
+
+    let [pokeArr, setpokeArr] = useState([]);
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+            .then(res => res.json())
+            .then(data => {
+                setpokeArr(data.results);
+            })
+    }, [])
+
+
+    return (
+        <>
+            <div className="main">
+                <img src={PokemonLogo} alt="" className="pokemon-logo" />
+                <div className="container">
+                    {pokeArr.map(obj => {
+                        return (
+                            <Pokemon key={obj.name} obj={obj} />
+                        )
+                    })}
+                </div>
+            </div>
+
+
+        </>
+
+    )
+}
+
+export default App;
---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+
+function Pokemon(props) {
+    let colors = {
+        bug: "#a8b820",
+        dark: "#705848",
+        dragon: "#7038f8",
+        electric: "#f8d030",
+        fairy: "#f8a0e0",
+        fighting: "#903028",
+        fire: "#f05030",
+        flying: "#a890f0",
+        ghost: "#705898",
+        grass: "#78c850",
+        ground: "#e0c068",
+        ice: "#98d8d8",
+        normal: "#a8a878",
+        poison: "#a040a0",
+        psychic: "#f85888",
+        rock: "#b8a038",
+        shadow: "#403246",
+        steel: "#403246",
+        unknown: "#68a090",
+        water: "#6890f0",
+
+    }
+    let obj = props.obj;
+    let pokemon = obj.name;
+    console.log(pokemon)
+    let [data, setData] = useState();
+    let [url, setUrl] = useState();
+    let [type, setType] = useState();
+    let [id, setId] = useState();
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+            .then(res => res.json())
+            .then(data => {
+                setData(data);
+                setUrl(data.sprites.front_default)
+                setType(data.types[0].type.name)
+                setId(data.id)
+            })
+    }, [])
+
+    return (
+        <div style={{ background: `${colors[type]}` }} className="pokemon">
+            <img src={url} alt="" className="pokemon-img" />
+            <div className="id">{"# "+ id}</div>
+            <div className="title">{pokemon}</div>
+            <div className="type">{"Type: "+type}</div>
+        </div>
+    )
+}
+
+export default Pokemon;
